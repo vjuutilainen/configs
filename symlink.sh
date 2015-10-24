c9b0aaaebfdccf64f711b17718e50768fab2419a
Ville Juutilainen <vjuutilainen@gmail.com>
2015-10-24 13:02:57 +0300

    added symlink script

---
+++ symlink.sh
@@ -0,0 +1,9 @@
+echo "Create symlinks inside user folder if they don't exist yet:"
+directory=${PWD}
+ln -shv $directory/.Rprofile ~/.Rprofile
+ln -shv $directory/.bash_profile ~/.bash_profile
+ln -shv $directory/.bashrc ~/.bashrc
+ln -shv $directory/.eslintrc ~/.eslintrc
+ln -shv $directory/.gitignore_global ~/.gitignore_global
+ln -shv $directory/.npmrc ~/.npmrc
+echo "Done."
